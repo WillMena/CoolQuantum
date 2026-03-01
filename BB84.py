053774a29d30fbcc63b2fd638e9a4735512ea1cb
Will <william_mena1@outlook.com>
2026-03-01 15:41:38 +0000

    update bb84

--- BB84.py
+++ BB84.py
@@ -1,1 +1,7 @@
-#start preparing GHZ state here
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import AerSimulator
+from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+
+
+
